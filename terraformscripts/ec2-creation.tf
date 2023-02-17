d053ab9d3ddd22e21be0d259d46b7be2104c0156
mssum91 <114442748+mssum91@users.noreply.github.com>
2023-02-18 00:48:45 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,16 +1,15 @@
 provider "aws"{
-  region = "ap-southeast-2"
+region = "eu-west-1"
 }
 
+resource "aws_instance" "EC2Instance"{
+  count = 1
+  ami = "ami-0acca42a314c5aee1"
+  instance_type = "t2.micro"
+  key_name = "mykp1"
+  security_groups = ["launch-wizard-1"]
+  tags{
+  Name = "tsttomserv"
+  }
 
-resource "aws_instance" "AWSInstance"{
-    ami = "ami-08cd0547bf85331e7"
-	instance_type = "t2.micro"
-	key_name = "devopstrainingmss"
-	security_groups = ["launch-wizard-2"]
-	tags = {
-	 Name = "tomcatservers"
-	}
 }
-
-
